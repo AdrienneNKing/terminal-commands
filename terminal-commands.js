d78bab4d8423e81f31ecb568c7b86ff8d1ab4eec
Adrienne <adriennenking@gmail.com>
2019-06-01 09:56:34 -0700

    code challenge

---
+++ terminal-commands.js
@@ -0,0 +1,28 @@
+module.exports.ls = () => {
+
+};
+
+const fs = require('fs');
+
+ module.exports.ls = () => {
+   fs.readdir('./', (err, files) => {
+     const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
+
+    console.log(filesToString);
+   });
+ };
+
+module.exports.touch = (input) => {
+  fs.writeFile(input, data, (err) => {
+    if (err) throw err;
+    console.log('Success');
+  });
+};
+
+module.exports.mkdir = (input) => {
+  fs.mkdir(input, { recursive:true }, (err) => {
+    if(err) throw err;
+  });
+};
